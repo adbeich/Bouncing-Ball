1f36a517eb0f50015cdfb9afbc8a6a09a53b85ee
adbeich <aicha_haidara_@hotmail.com>
2013-11-14 21:46:38 +0200

    Create sandman.js

---
+++ sandman.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+
+function Lammas() {
+	this.image=kuva;
+}
+
+function Aita(){
+	this.image=kuva;
+}
+
+function Tausta(){
+	this.image=kuva;
+}
+
+var lammas = new Lammas("", "Untitled.png");
+var Lampaat = new Array(lammas);
+var aita = new Aita("", "image.png");
+var tausta = new Tausta("", "tausta.png");
+
+$(document.)ready(function)) {
+	
+	
+	if()
+	//luodaa eka looppi
+	//ajastin lähtee käyntiin eli peli alkaa
+	for(int i=0; i<20; i++){
+		
+		//looppii lampaille
+		for(int j=0; j<30; j++){
+			//tässä kohtaa lampaat tulevat esille yksitellen 
+			if(/*aika on suurempi kuin 1min, tahti nopeentuu */){
+				
+			}
+		}
+	}
+	
+
+}
+
+
